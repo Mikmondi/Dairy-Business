47cf93c429bdc2fc28d900a4354f9fae3efbb6f6
Moringa School <mikmondi@gmail.comgit config --global user.name Mikmondigit config --global user.Mikmondi>
2021-03-15 05:38:01 +0300

    added javascript file and linked to html

---
+++ script.js
@@ -0,0 +1,12 @@
+var dailyTotal = function (shade2a,shade2b, shade2c, shade2d ) {
+var shade1a = 510;
+var shade2a = shade1a * 45;
+var shade1b = 308;
+var shade2b = shade1b * 45;
+var shade1c = 486;
+var shade2c = shade1c * 45;
+var shade1d = 572;
+var shade2d = shade1d * 45;
+console.log(shade2a + shade2b + shade2c + shade2d)
+};
+dailyTotal = document.getElementById("dailyTotal");
