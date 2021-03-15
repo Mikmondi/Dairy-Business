4db516c61b692713be90b4abd6f4d620dd23504f
Moringa School <mikmondi@gmail.comgit config --global user.name Mikmondigit config --global user.Mikmondi>
2021-03-15 06:27:32 +0300

    Initiated the inclusion of js elements to link with the second table

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ var shade2d = shade1d * 45;
 console.log(shade2a + shade2b + shade2c + shade2d)
 };
 dailyTotal = document.getElementById("dailyTotal");
+var weeklyTotal = function (shade4a, shade4b, shade4c, shade4d) {
+var shade3a = 3570;
+var shade4a = shade3a * 45;
+var shade3b = 2156;
+var shade4b = shade3b * 45;
+var shade3c = 3402
+var shade4c = shade3c * 45;
+var shade3d = 4004
+var shade4d = shade3d *45;
+
+}
