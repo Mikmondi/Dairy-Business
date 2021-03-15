f27d9dfa2373cdafe274b316d125a90a498c5633
Moringa School <mikmondi@gmail.comgit config --global user.name Mikmondigit config --global user.Mikmondi>
2021-03-15 07:04:49 +0300

    created link to the third table on js to provide functionality to the content

--- script.js
+++ script.js
@@ -21,4 +21,31 @@ var shade3d = 4004
 var shade4d = shade3d *45;
 console.log(shade4a + shade4b + shade4c + shade4d);
 weeklTotal = document.getElementById(weeklyTotal);
+};
+yearlyTotal = function (jan2, feb2, mar2, apr2, may2, jun2, jul2, aug2, sep2, oct2, nov2, dec2) {
+var jan1 = 58156;
+var jan2 = jan1 * 45;
+var feb1 = 54404;
+var feb2 = feb1 * 45;
+var mar1 = 58156;
+var mar2 = mar1 * 45;
+var apr1 = 56280;
+var apr2 = apr1 * 45;
+var may1 = 58156;
+var may2 = may1 * 45;
+var jun1 = 56280;
+var jun2 = jun1 * 45;
+var jul1 = 58156;
+var jul2 = jul1 * 45;
+var aug1 = 58156;
+var aug2 = aug1 * 45;
+var sep1 = 56280;
+var sep2 = sep1 * 45;
+var oct1 = 58156;
+var oct2 = var1 * 45;
+var nov1 = 56280;
+var nov2 = var1 * 45;
+var dec1 = 58156;
+var dec2 = dec1 * 45;
+console.log(jan2 + feb2 + mar2 + apr2 + may2 + jun2 + jul2 + aug2 + sep2 + oct2 + nov2 + dec2);
 }
